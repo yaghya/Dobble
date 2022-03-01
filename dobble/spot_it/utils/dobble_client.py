a7a208e8eae05b1893bc639b05c1530b9f3d8c97
yaghyavardhan <Yaghyavardhan.K@ril.com>
2022-03-01 19:04:41 +0530

    fixed client code

--- dobble/spot_it/utils/dobble_client.py
+++ dobble/spot_it/utils/dobble_client.py
@@ -2,35 +2,75 @@ import socket
 import pickle
 import time
 
+# from matplotlib.pyplot import bar_label
+
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = "10.18.121.219"
-        self.port = 6000
+        self.host = "ec2-13-233-87-61.ap-south-1.compute.amazonaws.com"
+        self.port = 8000
         self.addr = (self.host, self.port)
-        self.dobble = self.connect()
-        self.dobble = pickle.loads(self.dobble)
+        print(self.addr)
+        data_string = self.connect()
+        data_string =b"".join(data_string)
+        # print("recieved",data_string,len(data_string))
+        self.dobble = pickle.loads(data_string)
+        print("dobble")
 
     def connect(self):
         self.client.connect(self.addr)
-        return self.client.recv(4096*8)
+        self.client.settimeout(0.5)
+        print("trying to receive")
+        data = []
+        while True:
+            print("getting_packet")
+            try :
+                packet =self.client.recv(4*4096)
+                print("packet",len(packet))
+                if not packet: break
+                data.append(packet)
+            except Exception as e:
+                print(e)
+                break
+        self.client.settimeout(None)
+        return data
 
     def disconnect(self):
         self.client.close()
 
-    def send(self, data, pick=False):
+    def send(self, data, pick=True):
         """
         :param data: str
         :return: str
         """
         start_time = time.time()
-        while time.time() - start_time < 5:
+        while time.time() - start_time <8:
             try:
                 if pick:
+                    # print(data)
                     self.client.send(pickle.dumps(data))
                 else:
                     self.client.send(str.encode(data))
-                reply = self.client.recv(4096*8)
+                reply = []
+                self.client.settimeout(0.5)
+                # i=0
+                while True:
+                    try :
+                        # print(i)
+                        packet =self.client.recv(4*4096)
+                        print("packet",len(packet))
+                        if not packet: 
+                            print("no packet receieved")
+                            break
+                        reply.append(packet)
+                        # i+=1
+                        # if len(packet)<4096:
+                        #     break
+                    except Exception as e:
+                        print(e)
+                        break
+                self.client.settimeout(None)
+                reply =b"".join(reply)
                 try:
                     reply = pickle.loads(reply)
                     break
